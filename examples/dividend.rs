159e8da011147017e0e01503e233785ebdca4f62
Juan C. Yamacho H <jcyamacho@gmail.com>
2020-04-23 03:28:01 +0200

    refactor: update info

--- examples/dividend.rs
+++ examples/dividend.rs
@@ -2,8 +2,8 @@ use yahoo_dl::{chrono::Local, Event, Period, Result, Yahoo};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut dwl = Yahoo::new();
-    let res = dwl
+    let mut yahoo = Yahoo::new();
+    let res = yahoo
         .download::<Local>("MSFT", None, None, Period::Daily, Event::Dividend)
         .await?;
     println!("{}", res);
--- examples/history.rs
+++ examples/history.rs
@@ -2,8 +2,8 @@ use yahoo_dl::{chrono::Local, Event, Period, Result, Yahoo};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut dwl = Yahoo::new();
-    let res = dwl
+    let mut yahoo = Yahoo::new();
+    let res = yahoo
         .download::<Local>("GOOG", None, None, Period::Monthly, Event::History)
         .await?;
     println!("{}", res);
